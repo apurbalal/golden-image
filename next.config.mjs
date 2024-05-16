7bc1fcdf4673017d22da33dcb683c5e4e5e10836
Apurbalal Singha <apurbalal@mercari.com>
2024-05-16 21:03:28 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.module.rules.push({
+        test: /\.ttf$/,
+        use: "file-loader"
+      });
+
+      return config;
+    }
+
+    return config;
+  }
+};
 
 export default nextConfig;
